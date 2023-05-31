6f1426dd157ab4d53fca0629e536ddf52c869986
Saishiva Reddy Gatla <saishivareddy.gatla@gmail.com>
2023-05-31 14:34:17 +0530

    DWM1000

---
+++ dwm1000-twr-initiator/Debug/Core/Inc/platform/subdir.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/platform/deca_mutex.c \
+../Core/Inc/platform/deca_range_tables.c \
+../Core/Inc/platform/deca_sleep.c \
+../Core/Inc/platform/deca_spi.c \
+../Core/Inc/platform/port.c 
+
+OBJS += \
+./Core/Inc/platform/deca_mutex.o \
+./Core/Inc/platform/deca_range_tables.o \
+./Core/Inc/platform/deca_sleep.o \
+./Core/Inc/platform/deca_spi.o \
+./Core/Inc/platform/port.o 
+
+C_DEPS += \
+./Core/Inc/platform/deca_mutex.d \
+./Core/Inc/platform/deca_range_tables.d \
+./Core/Inc/platform/deca_sleep.d \
+./Core/Inc/platform/deca_spi.d \
+./Core/Inc/platform/port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/platform/%.o: ../Core/Inc/platform/%.c Core/Inc/platform/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g -DDEBUG -DUSE_HAL_DRIVER -DSTM32L152xE -c -I../Drivers/CMSIS/Include -I../Core/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32L1xx/Include -I../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy -I"D:/STM32CubeIDE/dwm1000-twr-initiator/Core/Inc/platform" -I"D:/STM32CubeIDE/dwm1000-twr-initiator/Core/Inc/decadriver" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
---
+++ dwm1000-twr-responder/Debug/Core/Src/subdir.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/gpio.c \
+../Core/Src/main.c \
+../Core/Src/spi.c \
+../Core/Src/stm32l1xx_hal_msp.c \
+../Core/Src/stm32l1xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32l1xx.c \
+../Core/Src/twr-responder.c \
+../Core/Src/usart.c 
+
+OBJS += \
+./Core/Src/gpio.o \
+./Core/Src/main.o \
+./Core/Src/spi.o \
+./Core/Src/stm32l1xx_hal_msp.o \
+./Core/Src/stm32l1xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32l1xx.o \
+./Core/Src/twr-responder.o \
+./Core/Src/usart.o 
+
+C_DEPS += \
+./Core/Src/gpio.d \
+./Core/Src/main.d \
+./Core/Src/spi.d \
+./Core/Src/stm32l1xx_hal_msp.d \
+./Core/Src/stm32l1xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32l1xx.d \
+./Core/Src/twr-responder.d \
+./Core/Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g -DDEBUG -DUSE_HAL_DRIVER -DSTM32L152xE -c -I../Drivers/CMSIS/Include -I../Core/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32L1xx/Include -I../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy -I"D:/STM32CubeIDE/dwm1000-twr-responder/Core/Inc/platform" -I"D:/STM32CubeIDE/dwm1000-twr-responder/Core/Inc/decadriver" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
