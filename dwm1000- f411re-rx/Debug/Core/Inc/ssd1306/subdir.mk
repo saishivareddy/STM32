3761dde7ed928b3f014e3b8cf3a765c85d7b702b
Saishiva Reddy Gatla <saishivareddy.gatla@gmail.com>
2023-06-19 12:02:28 +0530

    Updated

---
+++ dwm1000- f411re-rx/Debug/Core/Inc/ssd1306/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/ssd1306/fonts.c \
+../Core/Inc/ssd1306/ssd1306.c \
+../Core/Inc/ssd1306/test.c 
+
+OBJS += \
+./Core/Inc/ssd1306/fonts.o \
+./Core/Inc/ssd1306/ssd1306.o \
+./Core/Inc/ssd1306/test.o 
+
+C_DEPS += \
+./Core/Inc/ssd1306/fonts.d \
+./Core/Inc/ssd1306/ssd1306.d \
+./Core/Inc/ssd1306/test.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/ssd1306/%.o: ../Core/Inc/ssd1306/%.c Core/Inc/ssd1306/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"D:/STM32CubeIDE/dwm1000- f411re-rx/Core/Inc/decadriver" -I"D:/STM32CubeIDE/dwm1000- f411re-rx/Core/Inc/platform" -I"D:/STM32CubeIDE/dwm1000- f411re-rx/Core/Inc/ssd1306" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
--- oled/Debug/Core/Inc/subdir.mk
+++ oled/Debug/Core/Inc/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -21,13 +21,6 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Inc/%.o Core/Inc/%.su: ../Core/Inc/%.c Core/Inc/subdir.mk
+Core/Inc/%.o: ../Core/Inc/%.c Core/Inc/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L152xE -c -I../Core/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
-
-clean: clean-Core-2f-Inc
-
-clean-Core-2f-Inc:
-	-$(RM) ./Core/Inc/ssd1306.d ./Core/Inc/ssd1306.o ./Core/Inc/ssd1306.su ./Core/Inc/ssd1306_fonts.d ./Core/Inc/ssd1306_fonts.o ./Core/Inc/ssd1306_fonts.su ./Core/Inc/ssd1306_tests.d ./Core/Inc/ssd1306_tests.o ./Core/Inc/ssd1306_tests.su
-
-.PHONY: clean-Core-2f-Inc
 
